63ebb60ac33f05a25655fbd0ec050a7b8bc6fb7d
lingfe <13068326391@163.com>
2017-05-17 17:02:35 +0800

    update Signed-off-by: lingfe <13068326391@163.com>

--- 人人配送/smuyunDB.sql
+++ 人人配送/smuyunDB.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v10.00 Beta1
-MySQL - 5.5.40 : Database - smuyundb
+MySQL - 5.6.35-log : Database - smuyundb
 *********************************************************************
 */
 
@@ -21,48 +21,34 @@ USE `smuyundb`;
 DROP TABLE IF EXISTS `kuaiketab`;
 
 CREATE TABLE `kuaiketab` (
-  `kuaikeId` varchar(64) NOT NULL,
-  `kuaikeName` varchar(50) NOT NULL,
-  `kuaikePhone` varchar(11) NOT NULL,
-  `kuaikeAddress` varchar(100) NOT NULL,
-  `kuaikeShenfenF` varchar(100) NOT NULL,
-  `kuaikeShenfenZ` varchar(100) NOT NULL,
-  `kuaikeShouchiSFZ` varchar(100) NOT NULL,
-  `kuaikeStatus` int(4) NOT NULL,
-  `kuaikeShengqingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `kuaikeId` varchar(64) NOT NULL COMMENT '快客信息表标识',
+  `kuaikeName` varchar(50) NOT NULL COMMENT '快客名称',
+  `kuaikePhone` varchar(11) NOT NULL COMMENT '快客电话',
+  `kuaikeAddress` varchar(100) NOT NULL COMMENT '居住地址',
+  `kuaikeShenfenF` varchar(100) NOT NULL COMMENT '身份证反面',
+  `kuaikeShenfenZ` varchar(100) NOT NULL COMMENT '身份证正面',
+  `kuaikeShouchiSFZ` varchar(100) NOT NULL COMMENT '手持身份证照片',
+  `kuaikeStatus` tinyint(4) NOT NULL COMMENT '状态',
+  `kuaikeShengqingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '快客申请时间',
+  `password` varchar(100) NOT NULL COMMENT '快客密码',
+  `loginDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '登录时间',
   PRIMARY KEY (`kuaikeId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `kuaiketab` */
-
-/*Table structure for table `logintab` */
-
-DROP TABLE IF EXISTS `logintab`;
-
-CREATE TABLE `logintab` (
-  `loginId` varchar(64) NOT NULL,
-  `kuaikeId` int(10) NOT NULL,
-  `kuaikeName` varchar(11) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `loginDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`loginId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-/*Data for the table `logintab` */
 
 /*Table structure for table `qiangordertab` */
 
 DROP TABLE IF EXISTS `qiangordertab`;
 
 CREATE TABLE `qiangordertab` (
-  `qiangId` varchar(64) NOT NULL,
-  `xiaId` varchar(64) NOT NULL,
-  `kuaikeId` varchar(64) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `qiangDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `qiangId` varchar(64) NOT NULL COMMENT '快客抢单标识',
+  `xiaId` varchar(64) NOT NULL COMMENT '下单id',
+  `kuaikeId` varchar(64) NOT NULL COMMENT '快客id',
+  `status` tinyint(4) NOT NULL COMMENT '状态',
+  `qiangDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '抢单时间',
   PRIMARY KEY (`qiangId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `qiangordertab` */
 
@@ -71,21 +57,25 @@ CREATE TABLE `qiangordertab` (
 DROP TABLE IF EXISTS `xiaordertab`;
 
 CREATE TABLE `xiaordertab` (
-  `xiaId` varchar(64) NOT NULL,
-  `shopId` varchar(64) NOT NULL,
-  `kuaikeId` varchar(64) NOT NULL,
-  `kuaikeName` varchar(11) NOT NULL,
-  `kuaikePhone` varchar(100) NOT NULL,
-  `kuaikeAddress` varchar(100) NOT NULL,
-  `‘shouhuoName’` varchar(11) NOT NULL,
-  `‘shouhuoShone’` varchar(11) NOT NULL,
-  `‘shouhuoAddress’` varchar(100) NOT NULL,
-  `‘quhouDate’` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `‘kaikePrioes’` decimal(10,0) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `okDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `xiaId` varchar(64) NOT NULL COMMENT '快客订单标识',
+  `kuaikeName` varchar(11) NOT NULL COMMENT '快客名称',
+  `kuaikePhone` varchar(100) NOT NULL COMMENT '快客电话',
+  `kuaikeAddress` varchar(100) NOT NULL COMMENT '快客地址',
+  `‘shouhuoName’` varchar(11) NOT NULL COMMENT '收货人名称',
+  `‘shouhuoShone’` varchar(11) NOT NULL COMMENT '收货人电话',
+  `‘shouhuoAddress’` varchar(100) NOT NULL COMMENT '收货人地址',
+  `‘quhouDate’` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '取货时间',
+  `‘kaikePrioes’` decimal(10,0) NOT NULL COMMENT '快递费用',
+  `status` tinyint(4) NOT NULL COMMENT '状态',
+  `okDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '到达时间',
+  `shopName` varchar(64) NOT NULL COMMENT '商品名称',
+  `Shopzholiang` float NOT NULL COMMENT '商品重量',
+  `ShopNumer` int(11) NOT NULL COMMENT '库存',
+  `Shopprices` float NOT NULL COMMENT '商品价格',
+  `shopguige` int(11) NOT NULL COMMENT '商品规格',
+  `Shopdate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '上架时间',
   PRIMARY KEY (`xiaId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `xiaordertab` */
 

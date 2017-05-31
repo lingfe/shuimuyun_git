f96a39d6c8f06349e8eba555b9026ac6e43ea63c
yangjie <191873811@qq.com>
2017-05-31 18:51:11 +0800

    杨杰 2017年5月31日18:51:06
    Signed-off-by: yangjie <191873811@qq.com>

--- YuanMa/Smy_PC/src/com/yyf/controller/util/RequestMappingUtil.java
+++ YuanMa/Smy_PC/src/com/yyf/controller/util/RequestMappingUtil.java
@@ -8,7 +8,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yyf.model.City;
 import com.yyf.service.CityService;

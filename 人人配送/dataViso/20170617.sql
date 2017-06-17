604dd37a342e7905ad0c70e27cd897399e0d1ad7
lijie <13068326391@163.com>
2017-06-17 13:21:50 +0800

    update Signed-off-by: lijie <13068326391@163.com>

--- 人人配送/dataViso/20170617.sql
+++ 人人配送/dataViso/20170617.sql
@@ -1,7 +1,7 @@
 
 
 SELECT a.dfk,b.yjd,c.yqh,d.wpj FROM  
-	(SELECT COUNT(xiaId) AS dfk FROM xiaordertab WHERE STATUS=0 AND payment=0) a,
+	(SELECT COUNT(xiaId) AS dfk FROM xiaordertab WHERE STATUS=0 AND payment=0 AND kuaikeId='') a,
 	(SELECT COUNT(xiaId) AS yjd FROM xiaordertab WHERE STATUS=1 AND payment=1) b,
 	(SELECT COUNT(xiaId) AS yqh FROM xiaordertab WHERE STATUS=2 AND payment=1) c,
 	(SELECT COUNT(xiaId) AS wpj FROM xiaordertab WHERE STATUS=3 AND payment=1) d
